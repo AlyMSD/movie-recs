bff1196f558622ae8b1c1174a49376f5c5609584
AlyMSD <princeafkyt@gmail.com>
2021-12-31 19:37:23 -0600

    Fixed Position of Posters

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -7,12 +7,11 @@ const MovieList = (props) => {
 		<>
             <div className="d-flex flex-wrap justify-content-center">
                 {props.movies.map((movie, index) => (
-                    <div className='image-container p-2'>
-                        <h5 className='d-flex align-items-center justify-content-center'>{movie.Title}</h5>
-                        <img src={movie.Poster} alt={movie}></img>
+                    <div className='image-container' style={{margin: '10px', backgroundColor: 'black'}}>
+                        <img className='mx-auto' src={movie.Poster} alt={movie.Title}></img>
                         <div
 						onClick={() => props.handleFavoritesClick(movie)}
-						className='overlay d-flex align-items-center justify-content-center'
+						className='overlay' 
 					>
 						<FavoriteComponent />
 					</div>
